a9593a0e6ee08451b13a26cce89cdedd1f27d80d
Harikeshav R <r.harikeshav@gmail.com>
2025-03-19 08:40:11 -0400

    Add encoder test functions to the header file

--- Robot/src/utils.h
+++ Robot/src/utils.h
@@ -5,4 +5,8 @@ namespace robot
     float degrees_to_radians(int degrees);
 
     float degrees_to_radians(float degrees);
+
+    float encoder_test(int speed, int milliseconds, Motor motor_handler);
+
+    float encoder_test(int speed, float seconds, Motor motor_handler);
 }
