0f7c5cb5e446796877ccfa7cff2f34f11d1294ae
Harikeshav R <r.harikeshav@gmail.com>
2025-03-19 08:34:48 -0400

    Create CdS class

--- Robot/src/cds_cell.cpp
+++ Robot/src/cds_cell.cpp
@@ -1,6 +1,8 @@
 #include "cds_cell.h"
 #include <cmath>
 
+#include "FEHLCD.h"
+
 
 namespace robot
 {
@@ -11,15 +13,28 @@ namespace robot
     LightType CdsCell::detect_light()
     {
         float voltage = cds_cell.Value();
+        LCD.WriteLine(voltage);
 
-        if (std::fabs(voltage - RED_LIGHT_VOLTAGE) < TOLERANCE) {
+        if (voltage < RED_LIGHT_VOLTAGE)
+        {
+            // LCD.WriteLine("Red!");
             return RED_LIGHT;
-        } else if (std::fabs(voltage - BLUE_LIGHT_VOLTAGE) < TOLERANCE) {
+        }
+        else if (std::fabs(voltage - BLUE_LIGHT_VOLTAGE) < TOLERANCE)
+        {
+            // LCD.WriteLine("VBlue!");
+
             return BLUE_LIGHT;
-        } else if (std::fabs(voltage - NO_LIGHT_VOLTAGE) < TOLERANCE) {
+        }
+        else if (std::fabs(voltage - NO_LIGHT_VOLTAGE) < TOLERANCE)
+        {
+            // LCD.WriteLine("No light!");
             return NO_LIGHT;
-        } else {
-            return UNKNOWN;
+        }
+        else
+        {
+            // LCD.WriteLine("Unknown!");
+            return UNKNOWN_LIGHT;
         }
     }
 }
--- Robot/src/cds_cell.h
+++ Robot/src/cds_cell.h
@@ -7,14 +7,14 @@ enum LightType {
     RED_LIGHT,
     BLUE_LIGHT,
     NO_LIGHT,
-    UNKNOWN
+    UNKNOWN_LIGHT
 };
 
 // Using a blue filter - might switch to red
-const float RED_LIGHT_VOLTAGE = 1.118;
-const float BLUE_LIGHT_VOLTAGE = 0.591;
-const float NO_LIGHT_VOLTAGE = 3.296;
-const float TOLERANCE = 0.2;
+const float RED_LIGHT_VOLTAGE = .5;
+const float BLUE_LIGHT_VOLTAGE = .87;
+const float NO_LIGHT_VOLTAGE = 3;
+const float TOLERANCE = 0.08;
 
 namespace robot
 {
