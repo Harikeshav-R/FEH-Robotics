15ce1b16eeda0d96d7d152f20a119fe1357cf057
Harikeshav R <r.harikeshav@gmail.com>
2025-03-27 17:54:48 -0400

    Remove encoder test from utils

--- Robot/src/utils.cpp
+++ Robot/src/utils.cpp
@@ -13,29 +13,29 @@ namespace robot
         return (3.14 / 180) * degrees;
     }
 
-    void encoder_test(int speed, int milliseconds, Motor motor_handler)
-    {
-        motor_handler.move_forward(speed);
-        Sleep(milliseconds);
-        motor_handler.stop();
+//     void encoder_test(int speed, int milliseconds, Motor motor_handler)
+//     {
+//         motor_handler.move_forward(speed);
+//         Sleep(milliseconds);
+//         motor_handler.stop();
 
-        LCD.Write("Left encoder count: ");
-        LCD.WriteLine(motor_handler.left_encoder_counts());
+//         LCD.Write("Left encoder count: ");
+//         LCD.WriteLine(motor_handler.left_encoder_counts());
 
-        LCD.Write("Right encoder count: ");
-        LCD.WriteLine(motor_handler.right_encoder_counts());
-    }
+//         LCD.Write("Right encoder count: ");
+//         LCD.WriteLine(motor_handler.right_encoder_counts());
+//     }
 
-    void encoder_test(int speed, float seconds, Motor motor_handler)
-    {
-        motor_handler.move_forward(speed);
-        Sleep(seconds);
-        motor_handler.stop();
+//     void encoder_test(int speed, float seconds, Motor motor_handler)
+//     {
+//         motor_handler.move_forward(speed);
+//         Sleep(seconds);
+//         motor_handler.stop();
 
-        LCD.Write("Left encoder count: ");
-        LCD.WriteLine(motor_handler.left_encoder_counts());
+//         LCD.Write("Left encoder count: ");
+//         LCD.WriteLine(motor_handler.left_encoder_counts());
 
-        LCD.Write("Right encoder count: ");
-        LCD.WriteLine(motor_handler.right_encoder_counts());
-    }
+//         LCD.Write("Right encoder count: ");
+//         LCD.WriteLine(motor_handler.right_encoder_counts());
+//     }
 }
--- Robot/src/utils.h
+++ Robot/src/utils.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "motor.h"
-
 namespace robot
 {
     float degrees_to_radians(int degrees);
 
     float degrees_to_radians(float degrees);
 
-    void encoder_test(int speed, int milliseconds, Motor motor_handler);
+    // void encoder_test(int speed, int milliseconds, Motor motor_handler);
 
-    void encoder_test(int speed, float seconds, Motor motor_handler);
+    // void encoder_test(int speed, float seconds, Motor motor_handler);
 }
