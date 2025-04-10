eeef3294f7bb0db0b15c55f4e1ca867c9754a18b
Harikeshav R <r.harikeshav@gmail.com>
2025-04-10 19:12:08 -0400

    Add zelda song

--- Robot/src/utils.cpp
+++ Robot/src/utils.cpp
@@ -1,5 +1,5 @@
 #include "utils.h"
-#include "FEHLCD.h"
+
 
 namespace robot
 {
@@ -13,29 +13,57 @@ namespace robot
         return (3.14 / 180) * degrees;
     }
 
-//     void encoder_test(int speed, int milliseconds, Motor motor_handler)
-//     {
-//         motor_handler.move_forward(speed);
-//         Sleep(milliseconds);
-//         motor_handler.stop();
-
-//         LCD.Write("Left encoder count: ");
-//         LCD.WriteLine(motor_handler.left_encoder_counts());
-
-//         LCD.Write("Right encoder count: ");
-//         LCD.WriteLine(motor_handler.right_encoder_counts());
-//     }
-
-//     void encoder_test(int speed, float seconds, Motor motor_handler)
-//     {
-//         motor_handler.move_forward(speed);
-//         Sleep(seconds);
-//         motor_handler.stop();
-
-//         LCD.Write("Left encoder count: ");
-//         LCD.WriteLine(motor_handler.left_encoder_counts());
-
-//         LCD.Write("Right encoder count: ");
-//         LCD.WriteLine(motor_handler.right_encoder_counts());
-//     }
+    void sing_the_zelda_theme_song()
+    {
+        Buzzer.Tone(FEHBuzzer::A5, 1000);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 500);
+        Buzzer.Tone(FEHBuzzer::G4, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 500);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 500);
+        Buzzer.Tone(FEHBuzzer::G4, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 500);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 100);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 100);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 50);
+        Buzzer.Tone(FEHBuzzer::E4, 100);
+        Buzzer.Tone(FEHBuzzer::E4, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 500);
+        Buzzer.Tone(FEHBuzzer::E4, 750);
+        Buzzer.Tone(FEHBuzzer::A5, 100);
+        Buzzer.Tone(FEHBuzzer::A5, 50);
+        Buzzer.Tone(FEHBuzzer::B5, 50);
+        Buzzer.Tone(FEHBuzzer::Cs5, 50);
+        Buzzer.Tone(FEHBuzzer::D5, 50);
+        Buzzer.Tone(FEHBuzzer::E5, 1000);
+        Buzzer.Tone(FEHBuzzer::E5, 100);
+        Buzzer.Tone(FEHBuzzer::E5, 100);
+        Buzzer.Tone(FEHBuzzer::F5, 100);
+        Buzzer.Tone(FEHBuzzer::G5, 100);
+        Buzzer.Tone(FEHBuzzer::A6, 1000);
+        Buzzer.Tone(FEHBuzzer::A6, 100);
+        Buzzer.Tone(FEHBuzzer::A6, 100);
+        Buzzer.Tone(FEHBuzzer::G5, 100);
+        Buzzer.Tone(FEHBuzzer::F5, 100);
+        Buzzer.Tone(FEHBuzzer::G5, 250);
+        Buzzer.Tone(FEHBuzzer::F5, 100);
+        Buzzer.Tone(FEHBuzzer::E5, 1000);
+    }
 }
